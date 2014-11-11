25cbe324dd56a8af997b70135644bfa83d84033b
Bernard Mordan <bernardmordan@gmail.com>
2014-11-11 21:05:39 +0000

    basic socket counting setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var server = require('express')();
 var http = require('http').Server(server);
 var expressLayouts = require('express-ejs-layouts');
+var io = require('socket.io')(http);
 
 server.set('view engine', 'ejs');
 server.set('views',__dirname + '/views');
@@ -18,3 +19,20 @@ http.listen(port, function() {
 })
 
 module.exports = http;
+
+var voters = []
+
+
+io.on('connection', function(socket){
+  console.log(socket.id)
+  voters.push(socket)
+  io.sockets.emit('votersConnectedUpdate', {votersConnectedUpdate: voters.length})
+
+  socket.on('disconnect', function(){
+    console.log('disconnect')
+    var thisOne = voters.indexOf(socket)
+    voters.splice(thisOne,1)
+    io.sockets.emit('votersConnectedUpdate', {votersConnectedUpdate: voters.length})
+  })
+});
+
