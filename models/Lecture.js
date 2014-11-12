5c90575aa97d12db0d6a36361bfcf12948703615
Bernard Mordan <bernardmordan@gmail.com>
2014-11-12 12:46:08 +0000

    Initial Lecture model tests complete and passing

--- models/Lecture.js
+++ models/Lecture.js
@@ -5,5 +5,11 @@ function Lecture() {
 Lecture.prototype.addVoter = function(voter) {
 	this.voters.push(voter);
 };
-
+Lecture.prototype.removeVoter = function(voter) {
+  var here = this.voters.indexOf(voter)
+  if(here !== -1) this.voters.splice(here,1) 
+};
+Lecture.prototype.countVoters = function() {
+  return this.voters.length
+}
 module.exports = Lecture
--- spec/LectureSpec.js
+++ spec/LectureSpec.js
@@ -3,18 +3,46 @@ var Lecture = require ('../models/Lecture');
 
 
 describe ('Lecture', function() {
+  
   var lecture
 
+  beforeEach(function(){
+		lecture = new Lecture();	
+  });
+
 	it ('can hold voters', function() {
-		lecture = new Lecture();
 		expect(lecture.voters).toBeArray;
 	});
 
 	it('can add a voter', function() {
-		lecture = new Lecture;
 		lecture.addVoter("Anna");
 		expect(lecture.voters.length).toEqual(1);
+	});
+
+	it('can remove a voter', function(){
+    lecture.addVoter("Anna");
+    lecture.removeVoter("Anna");
+    expect(lecture.voters.length).toEqual(0);
+	});
+
+	it('if the voter is not amongst the voters the voters don\t change', function(){
+		lecture.addVoter("Anna");
+		lecture.removeVoter("Bernie");
+    expect(lecture.voters.length).toEqual(1);
+	});
+
+	it('removes the right voter', function(){
+    lecture.addVoter("Anna");
+    lecture.addVoter("Bernie");
+		lecture.removeVoter("Bernie");
+    expect(lecture.voters.indexOf("Anna")).toNotEqual(-1)
+    expect(lecture.voters.indexOf("Bernie")).toEqual(-1)
 	})
 
-}); 
+	it('knows how many voters are connected', function(){
+    lecture.addVoter("Anna");
+    lecture.addVoter("Bernie");
+    expect(lecture.countVoters()).toEqual(2)
+	})
 
+});
