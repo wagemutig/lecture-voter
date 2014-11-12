078b6b171c9b26ea17ae6f6cf7089f2afae73692
Bernard Mordan <bernardmordan@gmail.com>
2014-11-12 15:52:48 +0000

    shared set up for two routes rendered in ejs

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var server = require('express')();
 var http = require('http').Server(server);
+var expressLayouts = require('express-ejs-layouts');
+var io = require('socket.io')(http);
+
+server.set('view engine', 'ejs');
+server.set('views',__dirname + '/views');
+server.use(expressLayouts)
+server.use(require('express').static(__dirname + '/public'));
 
 server.get('/', function (req, res) {
-  res.send('<button>Start</button>');
+  res.render('index', { layout: 'layout'})
+});
+
+server.get('/vote', function (req, res) {
+  res.render('vote', { layout: 'layout'})
 });
 
 var port = process.env.PORT || 1337;
-server.listen(port, function() {
 
-  // var host= server.address().address
-  // var port = server.address().port
-
-  console.log('Hello Node at')
-
+http.listen(port, function() {
+  console.log('Time to vote at ' + port)
 })
-
-module.exports = server;
