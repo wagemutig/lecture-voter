1b473cbf5613d20d3640f0bacf0334a655d82fa1
Bernard Mordan <bernardmordan@gmail.com>
2014-11-13 15:41:51 +0000

    Interface up and down tests passing

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,8 +1,4 @@
 var Interface = require('../../lib/Interface')
 $(document).ready(function() {
-
-	$('button').on('click', function() {
-		window.location = ('/vote');
-	})
-
+  
 })
--- spec/InterfaceSpec.js
+++ spec/InterfaceSpec.js
@@ -13,4 +13,22 @@ describe('The Interface', function() {
     expect(interface.userVote).toEqual(0)
   });
 
+  it('has a plus button', function(){
+    expect(interface.plus).toEqual('#plus')
+  })
+
+  it('has a minus button', function(){
+    expect(interface.minus).toEqual('#minus')
+  })
+
+  it('you can down the userVote value', function(){
+    interface.downVote()
+    expect(interface.userVote).toEqual(-1)
+  })
+
+  it('you can up the userVote value', function(){
+    interface.upVote()
+    expect(interface.userVote).toEqual(1)
+  })   
+
 });
